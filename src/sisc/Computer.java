34685a76950916c46fca75b5ab6e15286a3cd589
altrisi <altrisi.trillosierra@gmail.com>
2022-12-24 15:30:27 +0100

    Inline `InstructionParser` into `Computer`

--- src/sisc/Computer.java
+++ src/sisc/Computer.java
@@ -1,5 +1,9 @@
 package sisc;
 
+import sisc.instructions.*;
+
+import static sisc.instructions.Instructions.*;
+
 public class Computer {
 	private final InstructionStorage instructions;
 	
@@ -13,7 +17,27 @@ public class Computer {
 		
 		while (true) {
             System.out.println("PC addr: " + PC.peekStr());
-			InstructionParser.execute(instructions.getInstructionAt(PC.next()));
+			execute(instructions.getInstructionAt(PC.next()));
 		}
+	}
+
+	// TODO make non-static and use stuff from the Computer instance once it's not so static
+	public static void execute(short instr) {
+		byte instrType = (byte)(instr >> 12 << 4);
+		switch (instrType) {
+			case OPS  -> LogicArithmetic.handle(instr);
+			case CMP  -> Compare.handle(instr);
+			case ADDI -> Immediate.handle(instr);
+			case LD, ST, LDB, STB -> MemoryInstr.handle(instr);
+			case JUMP -> Branching.branch(instr);
+			case JALR -> Branching.jalr(instr);
+			case MOVE -> Moving.handle(instr);
+			case IO   -> throw new UnsupportedOperationException("TODO: I/O");
+			default   -> unknown(instrType);
+		};
+	}
+	
+	private static void unknown(byte instr) {
+		System.out.println("Unknown instruction: " + Integer.toHexString(Byte.toUnsignedInt(instr)) + ", considering as NOP");
 	}
 }
